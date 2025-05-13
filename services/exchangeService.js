af89851d436bc78dd41e1f84d31b437d84fe8c99
Jai Krishna <102488496+KnightJai@users.noreply.github.com>
2025-05-13 16:12:15 +0000

    Add caching mechanism for currency conversion rates

--- services/exchangeService.js
+++ services/exchangeService.js
@@ -1,9 +1,20 @@
 const axios = require('axios');
+const cache = require('../utils/cache');
 
 const getConvertedAmount = async (from, to, amount, date) => {
   const baseUrl = process.env.EXCHANGE_API_URL;
   const accessKey = process.env.ACCESS_KEY;
 
+  const cacheKey = `${from}_${to}_${date || 'latest'}`;
+
+  // Check cache first
+  const cachedRate = cache.get(cacheKey);
+  if (cachedRate) {
+    console.log('Using cached rate');
+    return cachedRate * amount;
+  }
+
+  // If not cached, call API
   let url = `${baseUrl}/convert?access_key=${accessKey}&from=${from}&to=${to}&amount=${amount}`;
   if (date) {
     url += `&date=${date}`;
@@ -16,9 +27,13 @@ const getConvertedAmount = async (from, to, amount, date) => {
     throw new Error('Conversion failed. Check your API key or inputs.');
   }
 
-  return response.data.result;
+  const rate = response.data.info.quote;
+  cache.set(cacheKey, rate);
+
+  return rate * amount;
 };
 
 module.exports = { getConvertedAmount };
+
 
 
---
+++ utils/cache.js
@@ -0,0 +1,4 @@
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: process.env.CACHE_TTL || 3600 });
+
+module.exports = cache;
